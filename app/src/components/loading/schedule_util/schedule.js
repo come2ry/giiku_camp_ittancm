2b8949cc6b3e8c420a7a271c5a136e75aa7c43dd
Hiroki Fujii <hfujii@hisystron.com>
2021-03-21 05:37:15 +0900

    予定の並び順不具合を修正

--- app/src/components/loading/schedule_util/schedule.js
+++ app/src/components/loading/schedule_util/schedule.js
@@ -6,7 +6,21 @@ var SCHEDULE_UTIL = {
 
 
     // 予定表シャッフル
-    shuffle: function(schedule, ignore_sleep=true){
+    shuffle: function(schedule, ignore_sleep=true, start_at="07:00", end_at="23:00"){
+        // スリープブロックビルド
+        let start_at_int = SCHEDULE_UTIL.str2time(start_at);
+        if (ignore_sleep){
+            schedule.push({"title": "", "task_time_min": 0, "start_at": start_at, "is_fix": false});
+            schedule.push({"title": "", "task_time_min": SCHEDULE_UTIL.constants.DAY_MINUTES - SCHEDULE_UTIL.str2time(end_at), "start_at": end_at, "is_fix": false});
+        } else{
+            let sleep_count = Math.floor(start_at_int/60);
+            schedule.push({"title": "", "task_time_min": 0, "start_at": SCHEDULE_UTIL.time2str(0), "is_fix": false});
+            for (let i=1; i<sleep_count; i++){
+                schedule.push({"title": "", "task_time_min": 60, "start_at": SCHEDULE_UTIL.time2str(i * 60), "is_fix": false});
+            }
+            schedule.push({"title": "", "task_time_min": SCHEDULE_UTIL.constants.DAY_MINUTES - SCHEDULE_UTIL.str2time(end_at), "start_at": end_at, "is_fix": false});
+        }
+        
         // 基本処理
         schedule = SCHEDULE_UTIL.set_start_at_int(schedule);
         let sorted = SCHEDULE_UTIL._sort(schedule);
@@ -14,7 +28,7 @@ var SCHEDULE_UTIL = {
 
         // 予定をシャッフル
         let result = SCHEDULE_UTIL._shuffle_sorted_schedule(splited);
-        // console.log(result);
+        console.log(result);
         return result;
     },
 
@@ -34,14 +48,26 @@ var SCHEDULE_UTIL = {
         }
     },
 
+    // 文字列から分(int)
+    str2time: function(str_time){
+        let res = str_time.split(":");
+        return parseInt(res[0]) * 60 + parseInt(res[1]);
+    },
+
+    // 分(int)から文字列
+    time2str: function(min_time){
+        let hour = Math.floor(min_time/60);
+        let min = min_time % 60;
+        return hour + ":" + min;
+    },
+
     // 開始時刻にintを埋め込み
     set_start_at_int: function(schedule){
         let ret = schedule.map(function(s){
             if (s.start_at == ""){
                 s["start_at_int"] = "";
             } else{
-                let res = s.start_at.split(":");
-                s["start_at_int"] = parseInt(res[0]) * 60 + parseInt(res[1]);
+                s["start_at_int"] = SCHEDULE_UTIL.str2time(s.start_at);
             }
             return s;
         });
@@ -96,11 +122,18 @@ var SCHEDULE_UTIL = {
 
     // 開始時刻で並び替え
     _sort: function(schedule){
-        return schedule.sort(function(a, b) {
-            if (a.start_at_int > b.start__at_int) {
-                return -1;
-            } else {
+        let ret = schedule.sort(function(a, b) {
+            if (a.task_time_min > b.task_time_min) {
                 return 1;
+            } else {
+                return -1;
+            }
+        });
+        return ret.sort(function(a, b) {
+            if (a.start_at_int > b.start_at_int) {
+                return 1;
+            } else {
+                return -1;
             }
         });
     },
@@ -118,6 +151,18 @@ var SCHEDULE_UTIL = {
             let ret = SCHEDULE_UTIL.take_random_task(schedule.fixed, margin, true);
             result.push(schedule.in_fixed[i]);
             if (ret){
+                let add_min = 0;
+                ret.forEach(function(el, idx){
+                    if (idx == 0){
+                        let next = schedule.in_fixed[i].start_at_int + schedule.in_fixed[i].task_time_min;
+                        el["start_at_int"] = next;
+                        el["start_at"] = SCHEDULE_UTIL.time2str(next);
+                    } else{
+                        let next = ret[idx-1].start_at_int + ret[idx-1].task_time_min;
+                        el["start_at_int"] = next;
+                        el["start_at"] = SCHEDULE_UTIL.time2str(next);
+                    }
+                })
                 result = result.concat(ret);
             }
         }
