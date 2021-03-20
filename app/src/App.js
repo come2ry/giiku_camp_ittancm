278942b4fffb37a8e8fdaab1c5a477da04d04b0e
come2ry <raiki5100@gmail.com>
2021-03-20 14:47:55 +0900

    UPDATE: デフォルトの状態からカスタマイズ

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import FormWrapper from './components/form/Form';
+import LoadingWrapper from './components/loading/Loading';
+import ResultWrapper from './components/loading/Loading';
+import Header from './components/header/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	// URLとReactコンポーネントの結びつけをしている
+	<Router>
+		<Header/>
+		<div id="body">
+			<Switch>
+				<Route exact path='/' component={FormWrapper} />
+				<Route exact path='/loading' component={LoadingWrapper} />
+				<Route exact path='/result' component={ResultWrapper} />
+				{/* <Route exact component={Page404}/> */}
+			</Switch>
+		</div>
+	</Router>
+)
 
+// export defaultをすると、外部のファイルから
+// import App from './App';
+// みたいな感じで呼び出せるようになる
 export default App;
